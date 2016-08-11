f610fd99cf31bfcc2ae4e4285479e7fea736a3d3
Andy Scherzinger <info@andy-scherzinger.de>
2016-08-11 13:13:26 +0200

    initial poc (wip) for show/hide toolbar on scroll

--- src/main/java/com/owncloud/android/ui/fragment/ExtendedListFragment.java
+++ src/main/java/com/owncloud/android/ui/fragment/ExtendedListFragment.java
@@ -32,6 +32,7 @@ import android.support.annotation.StringRes;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.SearchView;
 import android.text.TextUtils;
@@ -377,6 +378,9 @@ public class ExtendedListFragment extends Fragment
 
         mListView = (ExtendedListView) (v.findViewById(R.id.list_root));
         mListView.setOnItemClickListener(this);
+        ViewCompat.setNestedScrollingEnabled(mListView,true);
+
+        //mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
         mListFooterView = inflater.inflate(R.layout.list_footer, null, false);
 
         mGridView = (GridViewWithHeaderAndFooter) (v.findViewById(R.id.grid_root));
@@ -385,6 +389,7 @@ public class ExtendedListFragment extends Fragment
         setGridViewColumns(1f);
 
         mGridView.setOnItemClickListener(this);
+        ViewCompat.setNestedScrollingEnabled(mGridView,true);
 
         mGridFooterView = inflater.inflate(R.layout.list_footer, null, false);
 
