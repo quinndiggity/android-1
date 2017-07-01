5e3a36f02315e4859faf58ff8ae4e6d569b050e1
Mario Danic <mario@lovelyhq.com>
2017-07-01 18:23:39 +0200

    Fix duplicates
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- src/main/java/com/owncloud/android/ui/activity/FolderSyncActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/FolderSyncActivity.java
@@ -189,8 +189,6 @@ public class FolderSyncActivity extends FileActivity implements FolderSyncAdapte
                 mediaFolders.addAll(MediaProvider.getVideoFolders(getContentResolver(), perFolderMediaItemLimit));
                 Log_OC.w(TAG, "Picture+Video Folders: " + mediaFolders.size());
 
-                //TODO properly merge image and video lists to remove duplicates
-
                 List<SyncedFolder> syncedFolderArrayList = mSyncedFolderProvider.getSyncedFolders();
                 List<SyncedFolder> currentAccountSyncedFoldersList = new ArrayList<>();
                 Account currentAccount = AccountUtils.getCurrentOwnCloudAccount(FolderSyncActivity.this);
@@ -243,11 +241,6 @@ public class FolderSyncActivity extends FileActivity implements FolderSyncAdapte
             } else {
                 result.add(createSyncedFolderFromMediaFolder(mediaFolder));
             }
-        }
-
-        // No media folder and thus always a custom folder
-        for (SyncedFolder syncedFolder : syncedFoldersMap.values()) {
-            result.add(createSyncedFolderWithoutMediaFolder(syncedFolder));
         }
 
         return result;
